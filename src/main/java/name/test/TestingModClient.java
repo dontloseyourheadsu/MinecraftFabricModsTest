4fb711e8ab9b697886f42d631dcb3245b6827bef
Jesus Alvarez Sombrerero <93137954+dontloseyourheadsu@users.noreply.github.com>
2024-03-31 20:37:03 -0600

    Initial commit

---
+++ src/main/java/name/test/TestingModClient.java
@@ -0,0 +1,10 @@
+package name.test;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestingModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
